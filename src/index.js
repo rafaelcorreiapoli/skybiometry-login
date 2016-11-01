881cfc1d0c24327b13dd89cb5f788180d0a12b32
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-11-01 01:43:50 -0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,193 @@
+import waterfall from 'promise-waterfall'
+
+export default class LoginBiometry {
+  ERROR_NO_PICTURE = 'ERROR_NO_PICTURE'
+  ERROR_NO_FACES = 'ERROR_NO_FACES'
+  ERROR_THRESHOLD = 'ERROR_THRESHOLD'
+  ERROR_NO_CANDIDATES = 'ERROR_NO_CANDIDATES'
+  NAMESPACE_SEPARATOR = '@'
+  constructor(client, namespace, options = { debug: false }) {
+    this.client = client
+    this.namespace = namespace
+    this.options = options
+  }
+
+  log(what) {
+    if (this.options.debug) {
+      console.log(what)
+    }
+  }
+
+  registerFaceForUser(uid, url, customOptions) {
+    this.log('Detectando face...');
+
+    /**
+     * First step: Detect a face in a photo and get it's temporaryId
+     */
+    const detect = () => this.client.faces.detect({
+      urls: url,
+      detector: 'aggressive',
+      attributes: 'none',
+      ...customOptions,
+    })
+
+    /**
+     * Second step: Save that tag to a specific user
+     */
+    const saveTag = (res) => {
+      return new Promise((resolve, reject) => {
+        if (!res.photos.length) {
+          console.log('throwing!')
+          reject(this.ERROR_NO_PICTURE);
+        }
+
+        const photo = res.photos[0];
+
+        if (!photo.tags.length) {
+          reject(this.ERROR_NO_FACES);
+        }
+
+        const tag = photo.tags[0]
+
+        const tempTagId = tag.tid;
+
+        this.client.tags.save({
+          uid: this._buildNamespaceForUser(uid),
+          tids: tempTagId,
+        })
+        .then(res => resolve(res))
+        .catch(err => reject(err))
+      })
+    }
+
+    /**
+     * Third step: Train that user
+     */
+    const trainFace = () => {
+      this.log('Treinando usuário')
+      return this.client.faces.train({
+        uids: this._buildNamespaceForUser(uid),
+      })
+    }
+
+
+    //  return waterfall([detect, saveTag, trainFace])
+
+    return this.client.faces.detect({
+      urls: url,
+      detector: 'aggressive',
+      attributes: 'none',
+      ...customOptions,
+    })
+    .then(res => {
+      if (!res.photos.length) {
+        console.log('throwing!')
+        throw new Error(this.ERROR_NO_PICTURE);
+      }
+
+      const photo = res.photos[0];
+
+      if (!photo.tags.length) {
+        throw new Error(this.ERROR_NO_FACES);
+      }
+
+      const tag = photo.tags[0]
+
+      const tempTagId = tag.tid;
+
+      return this.client.tags.save({
+        uid: this._buildNamespaceForUser(uid),
+        tids: tempTagId,
+      })
+    })
+    .then(res => {
+      return this.client.faces.train({
+        uids: this._buildNamespaceForUser(uid),
+      })
+    })
+  }
+
+  /**
+   * Helpers
+   */
+  _buildNamespaceForUser(user) {
+    return `${user}${this.NAMESPACE_SEPARATOR}${this.namespace}`
+  }
+  _getUidFromNamespacedString(namespacedString) {
+    const separatorIndex = namespacedString.indexOf(this.NAMESPACE_SEPARATOR)
+    if (separatorIndex === -1) {
+      throw new Error(`${this.NAMESPACE_SEPARATOR} not found`)
+    }
+    return namespacedString.substr(0, separatorIndex);
+  }
+
+  _buildNamespacedUserList(users = []) {
+    return users.map(uid => this._buildNamespaceForUser(uid))
+      .join(',')
+  }
+  authenticateUserByPhoto(usersToCompare, urls, threshold, customOptions) {
+    const uids = this._buildNamespacedUserList(usersToCompare)
+
+    this.log('Reconhecendo faces... uids->', uids);
+
+    const recognize = () => this.client.faces.recognize({
+      uids,
+      urls,
+      detector: 'aggressive',
+      attributes: 'none',
+      limit: 1,
+      ...customOptions,
+    })
+
+    const authenticate = (res) => {
+      if (!res.photos.length) {
+        throw new Error(this.ERROR_NO_PICTURE);
+      }
+
+      const photo = res.photos[0];
+
+      if (!photo.tags.length) {
+        throw new Error(this.ERROR_NO_FACES);
+      }
+
+      const tag = photo.tags[0]
+      const tempTagId = tag.tid
+
+      const candidates = tag.uids
+
+      if (!candidates.length) {
+        throw new Error(this.ERROR_NO_CANDIDATES)
+      }
+      candidates.map(candidate => ({
+        uid: this._getUidFromNamespacedString(candidate.uid),
+        confidence: candidate.confidence,
+      }))
+
+      const bestCandidate = candidates[0]
+      this.log(bestCandidate)
+      if (bestCandidate.confidence < threshold) {
+        throw new Error(this.ERROR_THRESHOLD)
+      }
+      return {
+        uid: this._getUidFromNamespacedString(bestCandidate.uid),
+        confidence: bestCandidate.confidence,
+      }
+    }
+
+    const saveTagForBestCandidate = ({ bestCandidate, tempTagId }) =>
+      new Promise((resolve, reject) => {
+        this.client.tags.save({
+          uid: bestCandidate.uid,
+          tids: tempTagId,
+        })
+        .then(() => resolve(bestCandidate.uid))
+        .catch(reject)
+      })
+
+    const trainFace = uid => this.client.faces.train({
+      uids: uid,
+    })
+
+    return waterfall([recognize, authenticate]) //  , saveTagForBestCandidate, trainFace])
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,258 @@
+'use strict';
+
+import chai from 'chai'
+import SkyBiometryLogin from '../src/index';
+import SkyBiometryClient from 'skybiometry-client'
+import nock from 'nock'
+
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should()
+
+const expect = chai.expect
+const assert = chai.assert
+
+const skyBiometryClient2 = {
+  faces: {
+    detect() {
+      return new Promise((resolve, reject) => {
+        resolve({
+          photos: [
+            // {
+            //   tags: [
+            //     {
+            //       tid: 'tid',
+            //     }
+            //   ]
+            // }
+          ]
+        })
+      })
+    },
+    train() {
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    }
+  },
+  tags: {
+    save() {
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    }
+  }
+}
+
+const skyBiometryClient = new SkyBiometryClient('key', 'secret')
+const skyBiometryLogin = new SkyBiometryLogin(skyBiometryClient, 'my-namespace')
+
+
+describe('Sky Biometry Client', function() {
+    it('should build correct string for user@namespace', function() {
+      const skyBiometryLogin = new SkyBiometryLogin(skyBiometryClient, 'my-namespace')
+      expect(skyBiometryLogin._buildNamespaceForUser('test')).to.equal('test@my-namespace')
+    })
+    it('should retrieve correct uid for given namespaced string', function() {
+      const skyBiometryLogin = new SkyBiometryLogin(skyBiometryClient, 'my-namespace')
+      expect(skyBiometryLogin._getUidFromNamespacedString('test@my-namespace')).to.equal('test')
+    })
+    it('should throw error for invalid namespaced string', function() {
+      const skyBiometryLogin = new SkyBiometryLogin(skyBiometryClient, 'my-namespace')
+      expect(() => skyBiometryLogin._getUidFromNamespacedString('testmy-namespace')).to.throw(Error)
+    })
+});
+
+describe('Register face for user', function() {
+  it('should throw error if no pictures return from api request', function() {
+    const uid = 'myUserId'
+    const url = 'http://placehold.it/400x400'
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .post(/faces\/detect/)
+      .reply(200, {
+        photos: []
+      });
+    nock('http://api.skybiometry.com/fc')
+      .get(/tags\/save/)
+      .reply(200);
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/train/)
+      .reply(200, {
+        status: 'success'
+      });
+
+    const p = skyBiometryLogin.registerFaceForUser(uid, url, customOptions)
+    return expect(p).to.be.rejectedWith('ERROR_NO_PICTURE')
+  })
+
+  it('should throw error if no tags return from api request', function () {
+    const uid = 'myUserId'
+    const url = 'http://placehold.it/400x400'
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .post(/faces\/detect/)
+      .reply(200, {
+        photos: [{
+          tags: []
+        }]
+      });
+    nock('http://api.skybiometry.com/fc')
+      .get(/tags\/save/)
+      .reply(200);
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/train/)
+      .reply(200, {
+        status: 'success'
+      });
+
+    const p = skyBiometryLogin.registerFaceForUser(uid, url, customOptions)
+    return expect(p).to.be.rejectedWith('ERROR_NO_FACES')
+  })
+
+
+
+  it('should resolve promise a tag were returned', function() {
+    const uid = 'myUserId'
+    const url = 'http://placehold.it/400x400'
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .post(/faces\/detect/)
+      .reply(200, {
+        photos: [{
+          tags: [{
+            tid: 'myTagId'
+          }]
+        }]
+      });
+    nock('http://api.skybiometry.com/fc')
+      .get(/tags\/save/)
+      .reply(200);
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/train/)
+      .reply(200, {
+        status: 'success'
+      });
+    const p = skyBiometryLogin.registerFaceForUser(uid, url, customOptions)
+    return p.should.eventually.deep.property('status', 'success')
+  })
+})
+
+
+describe('authenticateUserByPhoto', function() {
+  it('_buildNamespacedUserList', function() {
+    const uids = ['rafa', 'pedro', 'joao']
+
+    const value = skyBiometryLogin._buildNamespacedUserList(uids)
+    const expected = 'rafa@my-namespace,pedro@my-namespace,joao@my-namespace'
+    assert.equal(value, expected)
+  })
+
+  it('should throw error if no pictures return from api request', function() {
+    const usersToCompare = ['rafa', 'pedro', 'joao']
+    const urls = 'http://placehold.it/400x400'
+    const threshold = 70
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/recognize/)
+      .reply(200, {
+        photos: []
+      });
+
+    const p = skyBiometryLogin.authenticateUserByPhoto(usersToCompare, urls, threshold, customOptions)
+    return expect(p).to.be.rejectedWith('ERROR_NO_PICTURE')
+  })
+
+  it('should throw error if no tags return from api request', function () {
+    const usersToCompare = ['rafa', 'pedro', 'joao']
+    const urls = 'http://placehold.it/400x400'
+    const threshold = 70
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/recognize/)
+      .reply(200, {
+        photos: [{
+          tags: []
+        }]
+      });
+
+    const p = skyBiometryLogin.authenticateUserByPhoto(usersToCompare, urls, threshold, customOptions)
+    return expect(p).to.be.rejectedWith('ERROR_NO_FACES')
+  })
+
+
+
+  it('should throw error if no candidates return from api request', function () {
+    const usersToCompare = ['rafa', 'pedro', 'joao']
+    const urls = 'http://placehold.it/400x400'
+    const threshold = 70
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/recognize/)
+      .reply(200, {
+        photos: [{
+          tags: [{
+            uids: []
+          }]
+        }]
+      });
+
+    const p = skyBiometryLogin.authenticateUserByPhoto(usersToCompare, urls, threshold, customOptions)
+    return expect(p).to.be.rejectedWith('ERROR_NO_CANDIDATES')
+  })
+
+  it('should throw error if best candidate does not has sufficient confidence', function () {
+    const usersToCompare = ['rafa', 'pedro', 'joao']
+    const urls = 'http://placehold.it/400x400'
+    const threshold = 70
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/recognize/)
+      .reply(200, {
+        photos: [{
+          tags: [{
+            uids: [{
+              uid: 'rafa@my-namespace',
+              confidence: 69,
+            }]
+          }]
+        }]
+      });
+
+    const p = skyBiometryLogin.authenticateUserByPhoto(usersToCompare, urls, threshold, customOptions)
+    return expect(p).to.be.rejectedWith('ERROR_THRESHOLD')
+  })
+
+  it('should return user uid when authentication is valid', function() {
+    const usersToCompare = ['rafa', 'pedro', 'joao']
+    const urls = 'http://placehold.it/400x400'
+    const threshold = 70
+    const customOptions = {}
+
+    nock('http://api.skybiometry.com/fc')
+      .get(/faces\/recognize/)
+      .reply(200, {
+        photos: [{
+          tags: [{
+            uids: [{
+              uid: 'rafa@my-namespace',
+              confidence: 71,
+            }]
+          }]
+        }]
+      });
+
+    const p = skyBiometryLogin.authenticateUserByPhoto(usersToCompare, urls, threshold, customOptions)
+    return p.should.eventually.deep.equal({
+      uid: 'rafa',
+      confidence: 71,
+    })
+  })
+})
